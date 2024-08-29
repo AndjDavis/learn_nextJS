83cc702b8a37888377c8e73900ea03418a566e4f
AndjDavis <andi.idavis@gmail.com>
2024-08-29 15:43:35 -0600

    added metadata to the app

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,11 +1,16 @@
-import { Suspense } from 'react';
+import { Suspense } from "react";
+import { Metadata } from "next";
 
 import CardWrapper from "@/app/ui/dashboard/cards";
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
 
 import { lusitana } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+  title: "Home",
+};
 
 
 export default async function Page() {
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,11 +1,19 @@
-import { Suspense } from 'react';
-import Search from '@/app/ui/search';
-import Pagination from '@/app/ui/invoices/pagination';
-import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { lusitana } from '@/app/ui/fonts';
-import { fetchInvoicesPages } from '@/app/lib/data';
+import { Suspense } from "react";
+import { Metadata } from "next";
+
+import Search from "@/app/ui/search";
+import Pagination from "@/app/ui/invoices/pagination";
+import Table from "@/app/ui/invoices/table";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+
+import { lusitana } from "@/app/ui/fonts";
+import { fetchInvoicesPages } from "@/app/lib/data";
+
+export const metadata: Metadata = {
+  title: "Invoices",
+};
+
 
 export default async function Page(
   {
